6a8cfd3175f81fef39c8f8e0f66814a1cb2109a0
Ankit <ankit705yadav@gmail.com>
2023-11-12 16:57:24 +0530

    Donw with skill section moving onto experience sec

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -44,6 +44,79 @@ const Desc = styled.div`
   }
 `;
 
+const Skillscontainers = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 30px;
+  justify-content: center;
+  gap: 30px;
+`;
+
+const Skill = styled.div`
+  width: 100%;
+  max-width: 500px;
+  background-color: ${({ theme }) => theme.card};
+  border: 0.1px solid #854ce6;
+  border-radius: 16px;
+  padding: 10px 36px;
+
+  @media (max-width: 768px) {
+    max-width: 400px;
+    padding: 10px 36px;
+  }
+
+  @media (max-width: 500px) {
+    max-width: 330px;
+    padding: 10px 36px;
+  }
+`;
+
+const SkillTitle = styled.h2`
+  font-size: 28px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+  margin-bottom: 12px;
+  text-align: center;
+`;
+
+const SkillList = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 12px;
+  margin-bottom: 20px;
+`;
+
+const SkillItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  justify-content: center;
+  font-size: 16px;
+  color: ${({ theme }) => theme.text_primary};
+  border-radius: 12px;
+  border: 1px solid ${({ theme }) => theme.primary + 80};
+  padding: 12px 16px;
+  font-weight: 400;
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+    padding: 8px 12px;
+  }
+
+  @media (max-width: 500px) {
+    font-size: 14px;
+    padding: 6px 12px;
+  }
+`;
+
+const SkillImage = styled.img`
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+`;
+
 const Skills = () => {
   return (
     <Container id="skills">
@@ -52,7 +125,7 @@ const Skills = () => {
         <Desc>
           Some of my skills that i have been working on for last 3 years
         </Desc>
-        {/* <Skillscontainers>
+        <Skillscontainers>
           {skills.map((item) => (
             <Skill>
               <SkillTitle>{item.title}</SkillTitle>
@@ -66,7 +139,7 @@ const Skills = () => {
               </SkillList>
             </Skill>
           ))}
-        </Skillscontainers> */}
+        </Skillscontainers>
       </Wrapper>
     </Container>
   );
